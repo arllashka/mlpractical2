c995e7e559b997e19609731eba46cc33e3d6b28a
Arlan Kalin <arlan914@gmail.com>
2024-11-25 23:43:08 +0000

    fix: small fix

--- run_vgg_38_bn_res_batch.sh
+++ run_vgg_38_bn_res_batch.sh
@@ -1,2 +1,2 @@
-python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 256 --num_epochs 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 5 --experiment_name VGG_38_BN_RES_BATCH_experiment --use_gpu False --num_classes 100 --block_type 'conv_block_bn_res' --continue_from_epoch -1 --learning_rate 1e-3 
+python pytorch_mlp_framework/train_evaluate_image_classification_system.py --batch_size 256 --num_epochs 100 --seed 0 --num_filters 32 --num_stages 3 --num_blocks_per_stage 5 --experiment_name VGG_38_BN_RES_BATCH_experiment --use_gpu True --num_classes 100 --block_type 'conv_block_bn_res' --continue_from_epoch -1 --learning_rate 1e-3 
 
